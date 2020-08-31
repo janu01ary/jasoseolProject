088cc7a89e3ad76105ee0f84f4095390c60c8a72
aaa <janu01ary@gmail.com>
2020-08-31 22:54:38 +0900

    9

--- JssProject/wsgi.py
+++ JssProject/wsgi.py
@@ -14,6 +14,8 @@ import signal
 import sys
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JssProject.settings')
+
 try:
     application = get_wsgi_application()
     print 'WSGI without exception'
